174911029e5013d446f8cd38b3081dbc8edec51e
technikks <67446692+technikks@users.noreply.github.com>
2021-04-24 11:54:15 -0400

    Restarting files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+VALUES
+("Cheesburger", false),
+("Mushroom Burger", false),
+("Veggie Burger", false),
+("InNOut Burger", false),
+("Bacon Cheeseburger", false);
