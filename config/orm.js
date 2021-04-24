e92172179046888bcecdd19495e26146da443a9e
technikks <67446692+technikks@users.noreply.github.com>
2021-04-24 12:10:01 -0400

    Editted files

--- config/orm.js
+++ config/orm.js
@@ -20,22 +20,20 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
   const arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
+  // Loop through the keys and push the key/value as a string into array
   for (const key in ob) {
     let value = ob[key];
-    // check to skip hidden properties
+    // Check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      // If the string has spaces, add quotations
       if (typeof value === 'string' && value.indexOf(' ') >= 0) {
         value = `'${value}'`;
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(`${key}=${value}`);
     }
   }
 
-  // translate array of strings to a single comma-separated string
+  // Translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -44,7 +44,7 @@ router.put('/api/burgers/:id', function(req, res) {
     condition,
     function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
+        // If no rows were changed, then the ID must not exist, then display 404
         return res.status(404).end();
       }
       res.status(200).end();
@@ -57,7 +57,7 @@ router.delete('/api/burgers/:id', function(req, res) {
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+      // If no rows were changed, then the ID must not exist, then display 404
       return res.status(404).end();
     }
     res.status(200).end();
